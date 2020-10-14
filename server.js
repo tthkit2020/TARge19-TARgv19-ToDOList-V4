c731fd2a4ed7f39809204bea512343d26226ea09
Julia Aasmaa <my_e_10@hotmail.com>
2020-10-14 11:08:04 +0300

    add partials

--- server.js
+++ server.js
@@ -8,11 +8,19 @@ app.set("view engine", ejs);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let toDoList = [];
+
 app.get("/", (req, res) => {
     let day = date.getDate();
     let weekday = date.getWeekDay();
     console.log(day);
-    res.render("index.ejs", {date: weekday});
+    res.render("index.ejs", {date: weekday, toDoItems: toDoList});
+});
+
+app.post("/", (req, res) => {
+    let newTask = req.body.newTask;
+    toDoList.push(newTask);
+    res.redirect("/");
 });
 
 app.listen(3000, ()=> {
